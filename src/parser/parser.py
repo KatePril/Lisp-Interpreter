f74264e878407847dd2271644283246eb6787371
Lytvynenko Serhii <serrheylitvinenko@gmail.com>
2025-05-17 22:49:57 +0300

    Rewrite toParts to generators

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -1,32 +1,30 @@
 from Node import Node
+from typing import Generator
 
 KEYWORDS = {
   'let'
 }
 
-def toParts(source: str) -> list[str]:
+def toParts(source: str) -> Generator[str, None, None]:
   parts = source.replace(')', ' ) ').replace('(', ' ( ').strip().split()
-  result = []
   stack = []
   level = 0
   for part in parts:
     if part in KEYWORDS:
-      result.append('(')
+      yield '('
       stack.append(level + 1)
       level += 1
     if part == '(':
       level += 1
     elif part == ')':
       level -= 1
-    result.append(part)
+    yield part
     if len(stack) > 0 and stack[-1] == level and part == ')':
-      result.append(')')
-  return result
+      yield ')'
 
 def tokenize(source: str) -> Node:
-  parts = toParts(source)
   root = current = Node()
-  for part in parts:
+  for part in toParts(source):
     if part == '(':
       node = Node(current)
       current.childs.append(node)
