9d72c0094c946042b250f6515886de813986ced0
Kateryna Prylutska <prylutska.kateryna@lll.kpi.ua>
2025-05-18 22:20:49 +0300

    Improve code according to PEP 8

--- src/parser/node.py
+++ src/parser/node.py
@@ -1,8 +1,14 @@
+"""This module provides Node class for parsing"""
+
 from dataclasses import dataclass, field
 from typing import Union
 
+
 @dataclass
 class Node:
+    """
+    This class holds the parsed data
+    """
     parent: Union['Node', None] = field(default=None)
     tokens: list[str] = field(default_factory=list)
     children: list['Node'] = field(default_factory=list)
--- src/parser/parser.py
+++ src/parser/parser.py
@@ -1,37 +1,44 @@
-from src.parser.Node import Node
+"""This module provides methods for parsing Lisp code"""
+
 from typing import Generator
+from src.parser.node import Node
 
 KEYWORDS = {
-  'let'
+    'let'
 }
 
 def _to_parts(source: str) -> Generator[str, None, None]:
-  parts = source.replace(')', ' ) ').replace('(', ' ( ').strip().split()
-  stack = []
-  level = 0
-  for part in parts:
-    if part in KEYWORDS:
-      stack.append(level + 1)
-      level += 1
-      yield '('
-    if part == '(':
-      level += 1
-    elif part == ')':
-      level -= 1
-    yield part
-    if len(stack) > 0 and stack[-1] == level and part == ')':
-      stack.pop()
-      yield ')'
+    parts = source.replace(')', ' ) ').replace('(', ' ( ').strip().split()
+    stack = []
+    level = 0
+    for part in parts:
+        if part in KEYWORDS:
+            stack.append(level + 1)
+            level += 1
+            yield '('
+        if part == '(':
+            level += 1
+        elif part == ')':
+            level -= 1
+        yield part
+        if len(stack) > 0 and stack[-1] == level and part == ')':
+            stack.pop()
+            yield ')'
 
 def tokenize(source: str) -> Node:
-  root = current = Node()
-  for part in _to_parts(source):
-    if part == '(':
-      node = Node(current)
-      current.children.append(node)
-      current = node
-    elif part == ')':
-      current = current.parent
-    else:
-      current.tokens.append(part)
-  return root.children[0]
+    """
+    This method tokenizes the Lisp code
+    :param source: the string with Lisp code
+    :return:
+    """
+    root = current = Node()
+    for part in _to_parts(source):
+        if part == '(':
+            node = Node(current)
+            current.children.append(node)
+            current = node
+        elif part == ')':
+            current = current.parent
+        else:
+            current.tokens.append(part)
+    return root.children[0]
