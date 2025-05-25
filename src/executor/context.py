348ffd14ff2c0bfa700b6bd4112c1f0fc29322c1
Kateryna Prylutska <prylutska.kateryna@lll.kpi.ua>
2025-05-25 17:04:06 +0300

    Add  docstrings

--- src/executor/context.py
+++ src/executor/context.py
@@ -1,7 +1,23 @@
 class Context(dict):
-    def __int__(self, params=(), args=(), parent_context=None):
+    """
+    Context with variables and functions definitions.
+    """
+
+    def __init__(self, params=(), args=(), parent_context=None):
+        """
+        Constructor.
+        :param params: variables names
+        :param args: variables definitions
+        :param parent_context: the link to the parent context
+        """
+        super(Context, self).__init__()
         self.update(zip(params, args))
         self.parent_context = parent_context
 
     def find(self, name):
+        """
+        Returns variable definition by name.
+        :param name: variable name
+        :return:
+        """
         return self if name in self.keys() else self.parent_context.find(name)
