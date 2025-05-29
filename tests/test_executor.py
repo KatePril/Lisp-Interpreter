93d95b3e7ba535b624b82c6fa82154c86d02ff4e
Kateryna Prylutska <prylutska.kateryna@lll.kpi.ua>
2025-05-29 23:26:35 +0300

    Refactor tests

--- tests/test_executor.py
+++ tests/test_executor.py
@@ -13,68 +13,76 @@ class MyTestCase(unittest.TestCase):
     This class tests if executor works correctly
     """
     @patch('sys.stdout', new_callable=StringIO)
-    def test_one(self, mock_stdout):
+    def test_execution_with_setf(self, mock_stdout):
         """
-        Test case one
+        Tests execution of code with setf
         :param mock_stdout:
         :return:
         """
-        execute_node_iterative(tokenize("""
+        code = """
             (let ((x 10)
                   (y 20))
                 (setf z (+ x y))
                 (print z))
-            """))
+        """
+        tokens = tokenize(code)
+        execute_node_iterative(tokens)
         self.assertEqual(mock_stdout.getvalue().strip(), "30")
 
     @patch('sys.stdout', new_callable=StringIO)
-    def test_two(self, mock_stdout):
+    def test_execution_with_if_true(self, mock_stdout):
         """
-        Test case two
+        Tests execution of code with if (if condition is true)
         :param mock_stdout:
         :return:
         """
-        execute_node_iterative(tokenize("""
-                (let ((x 14))
-                (if (evenp x)
-                  (print x)))
-                """))
+        code = """
+            (let ((x 14))
+            (if (evenp x)
+              (print x)))
+        """
+        tokens = tokenize(code)
+        execute_node_iterative(tokens)
         self.assertEqual(mock_stdout.getvalue().strip(), "14")
 
     @patch('sys.stdout', new_callable=StringIO)
-    def test_three(self, mock_stdout):
+    def test_execution_with_if_false(self, mock_stdout):
         """
-        Test case three
+        Tests execution of code with if (if condition is false)
         :param mock_stdout:
         :return:
         """
-        execute_node_iterative(tokenize("""
-                    (let ((x 15))
-                    (if (evenp x)
-                      (print x)))
-                    """))
+        code = """
+            (let ((x 15))
+            (if (evenp x)
+              (print x)))
+        """
+        tokens = tokenize(code)
+        execute_node_iterative(tokens)
         self.assertEqual(mock_stdout.getvalue().strip(), "")
 
     @patch('sys.stdout', new_callable=StringIO)
-    def test_four(self, mock_stdout):
+    def test_execution_with_loop(self, mock_stdout):
         """
-        Test case four
+        Test s execution of code with loop
         :param mock_stdout:
         :return:
         """
-        execute_node_iterative(tokenize("""
-                (
-                    let (
-                        (sum 0)
-                    )
-                    (loop for i from 1 to 10 by 2 do
-                        (
-                            setf sum (+ sum i)
-                        )
-                    )
-                    (print sum)
+        code = """
+            (
+                let (
+                    (sum 0)
                 )
-                """))
+                (loop for i from 1 to 10 by 2 do
+                    (
+                        setf sum (+ sum i)
+                    )
+                )
+                (print sum)
+            )
+        """
+        tokens = tokenize(code)
+        execute_node_iterative(tokens)
         self.assertEqual(mock_stdout.getvalue().strip(), "25")
 
 if __name__ == '__main__':
