a54c9e51ce7acd669140db9617ed50da3c674f7a
generalmaks <schrinan@gmail.com>
2025-05-31 23:33:46 +0300

    Refactored reader

--- src/reader/reader.py
+++ src/reader/reader.py
@@ -3,17 +3,21 @@ CLI entry point for the Lisp interpreter.
 """
 
 import sys
+import os
 from src.parser.parser import tokenize
 from src.executor.executor import execute_node_iterative
 
 def main():
     """Read a Lisp file, tokenize, execute, and print the result."""
     if len(sys.argv) > 1:
+        if not os.path.isfile(sys.argv[1]):
+            print("File was not found.")
+            return
         with open(sys.argv[1], "r", encoding="utf-8") as f:
             source = f.read()
+            node = tokenize(source)
+            result = execute_node_iterative(node)
+            print(result)
     else:
         print("You did not enter your filepath.")
         return
-    node = tokenize(source)
-    result = execute_node_iterative(node)
-    print(result)
